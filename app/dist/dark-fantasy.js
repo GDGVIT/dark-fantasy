4c718e98f38c0f2f594f7f0c320ef23a0c66bdfd
t2013anurag <tiwari.anurag126@gmail.com>
2015-03-29 20:00:54 +0530

    New Image Change

--- app/dist/dark-fantasy.js
+++ app/dist/dark-fantasy.js
@@ -245,16 +245,6 @@ dark.filter('htmlLinky', function($filter) {
     }
 });
 
-dark.controller('NavController', ['$scope', function ($scope) {
-	$scope.nav = [
-		{'name': 'About Us', 'action': '1'},
-		{'name': 'What We Do', 'action': '2'},
-		{'name': 'Our Works', 'action': '3'},
-		{'name': 'Team', 'action': '4'},
-		{'name': 'Contact Us', 'action': '5'}
-	];
-	console.log($scope.nav);
-}])
 dark.controller("NewsController", function ($http, $timeout, $filter, $log, $sce, Config, NavService) {
     var vm = this;
     NavService.setNavTab(1);
