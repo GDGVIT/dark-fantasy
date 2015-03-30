81fdc4b263002ff6a65a0d51784268a083d814ab
sahanarula <sahiln123@gmail.com>
2015-03-30 18:28:04 +0530

    added navbar scroll points

--- app/controllers/navbar.js
+++ app/controllers/navbar.js
@@ -1,10 +1,10 @@
 dark.controller('NavController', ['$scope', function ($scope) {
 	$scope.nav = [
-		{'name': 'About Us'},
-		{'name': 'What We Do'},
-		{'name': 'Our Works'},
-		{'name': 'Team'},
-		{'name': 'Contact Us'}
+		{'name': 'About Us', 'action': '1'},
+		{'name': 'What We Do', 'action': '2'},
+		{'name': 'Our Works', 'action': '3'},
+		{'name': 'Team', 'action': '4'},
+		{'name': 'Contact Us', 'action': '5'}
 	];
 	console.log($scope.nav);
 }])
--- app/dist/dark-fantasy.js
+++ app/dist/dark-fantasy.js
@@ -96,11 +96,11 @@ dark.controller('AboutController', function ($http, Config, $scope) {
 
 dark.controller('NavController', ['$scope', function ($scope) {
 	$scope.nav = [
-		{'name': 'About Us'},
-		{'name': 'What We Do'},
-		{'name': 'Our Works'},
-		{'name': 'Team'},
-		{'name': 'Contact Us'}
+		{'name': 'About Us', 'action': '1'},
+		{'name': 'What We Do', 'action': '2'},
+		{'name': 'Our Works', 'action': '3'},
+		{'name': 'Team', 'action': '4'},
+		{'name': 'Contact Us', 'action': '5'}
 	];
 	console.log($scope.nav);
 }])
